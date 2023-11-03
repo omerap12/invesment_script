8ca3beedfc6c4638fa000113614582084c73c401
Omer <omerap12@gmail.com>
2023-11-03 14:17:57 +0200

    Change -m message

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import argparse
 if __name__ == "__main__":
     money_bag = Omberbag()
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m',"--MONEY", help="Display the amount of money to invest in eash security", type=int)
+    parser.add_argument('-m',"--MONEY", help="Amount of money to invest in eash security", type=int)
     parser.add_argument('-info',"--INFO", help="Show securities info", action='store_true')
     args = parser.parse_args()
     
@@ -13,6 +13,3 @@ if __name__ == "__main__":
         money_bag.money_to_invest(args.MONEY)
     if args.INFO:
         money_bag.print_securities()
-
-    
-    
